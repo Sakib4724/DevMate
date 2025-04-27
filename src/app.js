3afb2372951dfc6cf219a08b758fd1f92f7e5524
Sakib <sakibshaikh2425@gmail.com>
2025-04-27 17:49:49 +0530

    Handle GET, POST, DELETE requests for '/user'

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from DevMate Test!")
-})
+app.get("/user", (req, res) => {
+    res.send("User data fetched successfully!")
+});
+
+app.post("/user", (req, res) => {
+    res.send("User data saved to the database!")
+});
+
+app.delete("/user", (req, res) => {
+    res.send("User data deleted from the database!")
+});
 
 app.listen(3000, () => {
     console.log("DevMate Server is running on port 3000")
