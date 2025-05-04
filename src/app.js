0bbba0c38a17254c1324db3c228355f748daafd7
Sakib <sakibshaikh2425@gmail.com>
2025-05-04 17:16:58 +0530

    Encrypt password & validateSignUpData

--- src/app.js
+++ src/app.js
@@ -1,22 +1,61 @@
 const express = require("express");
 const app = express();
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 require('dotenv').config();
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    //Creating a new instance of the user model
-    const user = new User(req.body);
-
     try {
+        //Validation of data
+        validateSignUpData(req);
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        //Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        //Creating a new instance of the user model
+        const user = new User({
+            firstName, lastName, emailId, password: passwordHash
+        });
         await user.save();
         res.send("User added successfully!")
     } catch (err) {
-        res.status(400).send("Error saving the user: " + err.message);
+        res.status(400).send("ERROR: " + err.message);
     }
 });
+
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        if (!validator.isEmail(emailId)) {
+            throw new Error("Invalid emailId: " + emailId);
+        }
+
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
+            throw new Error("Invalid Credentials!");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (isPasswordValid) {
+            res.send("Login successful!");
+        } else {
+            throw new Error("Invalid Credentials!");
+        }
+
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+})
+
 
 // Get user by email
 app.get("/user", async (req, res) => {
