4f95080b09b6c6c4f950dfb1bb3807aa5a41ed8d
Sakib <sakibshaikh2425@gmail.com>
2025-05-18 10:20:45 +0530

    using express router

--- src/app.js
+++ src/app.js
@@ -1,153 +1,19 @@
 const express = require("express");
 const app = express();
-const validator = require("validator");
-const bcrypt = require("bcrypt");
 require('dotenv').config();
 const connectDB = require("./config/database");
-const User = require("./models/user");
-const { validateSignUpData } = require("./utils/validation");
 const cookieParser = require("cookie-parser");
-const { userAuth } = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/signup", async (req, res) => {
-    try {
-        //Validation of data
-        validateSignUpData(req);
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
-        const { firstName, lastName, emailId, password } = req.body;
-
-        //Encrypt the password
-        const passwordHash = await bcrypt.hash(password, 10);
-
-        //Creating a new instance of the user model
-        const user = new User({
-            firstName, lastName, emailId, password: passwordHash
-        });
-        await user.save();
-        res.send("User added successfully!")
-    } catch (err) {
-        res.status(400).send("ERROR: " + err.message);
-    }
-});
-
-app.post("/login", async (req, res) => {
-    try {
-        const { emailId, password } = req.body;
-
-        if (!validator.isEmail(emailId)) {
-            throw new Error("Invalid emailId: " + emailId);
-        }
-
-        const user = await User.findOne({ emailId: emailId });
-        if (!user) {
-            throw new Error("Invalid Credentials!");
-        }
-
-        const isPasswordValid = await user.validatePassword(password);
-
-        if (isPasswordValid) {
-            const token = await user.getJWT();
-
-            res.cookie("token", token, {
-                expires: new Date(Date.now() + 24 * 3600000),
-            });
-            res.send("Login successful!");
-        } else {
-            throw new Error("Invalid Credentials!");
-        }
-    } catch (err) {
-        res.status(400).send("ERROR: " + err.message);
-    }
-})
-
-app.get("/profile", userAuth, async (req, res, next) => {
-    try {
-        const user = req.user;
-        res.send(user);
-    }
-    catch (err) {
-        res.status(400).send("ERROR: " + err.message);
-    }
-});
-
-app.post("/sendConnectionRequest", userAuth, async (req, res, next) => {
-    const user = req.user;
-    //Sending a connection request
-    console.log("Sending a connection request");
-    res.send(user.firstName + " sent the connection request!");
-});
-
-// Get user by email
-app.get("/user", async (req, res) => {
-    const userEmail = req.body.emailId;
-
-    try {
-        const users = await User.find({ emailId: userEmail });
-
-        if (users.length === 0) {
-            res.status(404).send("User not found");
-        } else {
-            res.send(users);
-        }
-    } catch (err) {
-        res.status(400).send("Something went wrong");
-    }
-});
-
-// Feed API - GET /feed - get all the users from the database
-app.get("/feed", async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (err) {
-        res.status(400).send("Something went wrong");
-    }
-});
-
-//Delete a user from the database
-app.delete("/user", async (req, res) => {
-    const userId = req.body.userId;
-
-    try {
-        //const user = await User.findByIdAndDelete({ _id: userId});
-        const user = await User.findByIdAndDelete(userId);
-        res.send("User deleted successfully");
-    } catch (err) {
-        res.status(400).send("Something went wrong");
-    }
-});
-
-app.patch("/user/:userId", async (req, res) => {
-    const userId = req.params?.userId;
-    const data = req.body;
-
-    try {
-        const ALLOWED_UPDATES = [
-            "photoUrl", "about", "gender", "age", "skills"
-        ];
-
-        const isUpdateAllowed = Object.keys(data).every((k) =>
-            ALLOWED_UPDATES.includes(k)
-        );
-
-        if (!isUpdateAllowed) {
-            throw new Error("Update not allowed");
-        }
-
-        if (data?.skills.length > 10) {
-            throw new Error("Skills can not be more than 10");
-        }
-
-        //const user = await User.findByIdAndUpdate(userId, data);
-        const user = await User.findByIdAndUpdate({ _id: userId }, data, { runValidators: true });
-        res.send("User updated successfully");
-    } catch (err) {
-        res.status(400).send("Failed to Update: " + err.message);
-    }
-});
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 connectDB().then(() => {
     console.log("Database connection established..");
---
+++ src/routes/auth.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const authRouter = express.Router();
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const { validateSignUpData } = require("../utils/validation");
+
+authRouter.post("/signup", async (req, res) => {
+    try {
+        //Validation of data
+        validateSignUpData(req);
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        //Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        //Creating a new instance of the user model
+        const user = new User({
+            firstName, lastName, emailId, password: passwordHash
+        });
+        await user.save();
+        res.send("User added successfully!")
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+authRouter.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        if (!validator.isEmail(emailId)) {
+            throw new Error("Invalid emailId: " + emailId);
+        }
+
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
+            throw new Error("Invalid Credentials!");
+        }
+
+        const isPasswordValid = await user.validatePassword(password);
+
+        if (isPasswordValid) {
+            const token = await user.getJWT();
+
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 24 * 3600000),
+            });
+            res.send("Login successful!");
+        } else {
+            throw new Error("Invalid Credentials!");
+        }
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+module.exports = authRouter;
