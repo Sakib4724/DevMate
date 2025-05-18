5ce079c120964dd32b3f9a406154e82d72b5d0a2
Sakib <sakibshaikh2425@gmail.com>
2025-05-18 11:50:23 +0530

    Add patch api "/profile/edit"

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,9 +1,9 @@
 const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const {firstName, lastName, emailId, password} = req.body;
+    const { firstName, lastName, emailId, password } = req.body;
 
-    if(!firstName || !lastName) {
+    if (!firstName || !lastName) {
         throw new Error("Name is not valid!");
     } else if (!validator.isEmail(emailId)) {
         throw new Error("Email is not valid!");
@@ -12,6 +12,26 @@ const validateSignUpData = (req) => {
     }
 };
 
+const validateEditProfileData = (req) => {
+    const allowedEditFields = [
+        "firstName",
+        "lastName",
+        "emailId",
+        "photoUrl",
+        "gender",
+        "age",
+        "about",
+        "skills"
+    ];
+
+    const isEditAllowed = Object.keys(req.body).every((field) =>
+        allowedEditFields.includes(field)
+    );
+
+    return isEditAllowed;
+};
+
 module.exports = {
     validateSignUpData,
+    validateEditProfileData,
 };
