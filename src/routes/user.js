87c8b388e2aec5827dae7c54a70361a4317c3388
Sakib <sakibshaikh2425@gmail.com>
2025-07-16 17:08:30 +0530

    "user/connections" api and use of ref & populate

--- src/routes/user.js
+++ src/routes/user.js
@@ -12,7 +12,8 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested",
-        });
+        }).populate("fromUserId", "firstName lastName");
+        //.populate("fromUserId", ["firstName", "lastName"]);
 
         res.json({
             message: "Data fetched successfully",
@@ -21,6 +22,32 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     }
     catch (err) {
         res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" }
+            ],
+        }).populate("fromUserId", ["firstName", "lastName"])
+            .populate("toUserId", ["firstName", "lastName"]);
+
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId;
+            }
+
+            return row.fromUserId;
+        });
+
+        res.json({ data });
+    }
+    catch (err) {
+        res.status(400).send({ message: err.message });
     }
 });
 
